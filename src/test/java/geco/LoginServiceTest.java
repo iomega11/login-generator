f6bc6ab15a2ad1f756b34e19523c14f43e65381f
iomega11 <45142835+iomega11@users.noreply.github.com>
2019-11-19 08:18:20 +0100

    Deleted AppTest.java, added LoginServiceTest.java for tests.

---
+++ src/test/java/geco/LoginServiceTest.java
@@ -0,0 +1,22 @@
+package geco;
+
+import org.junit.Test;
+
+public class LoginServiceTest {
+
+    @Test
+    public void loginExists() {
+    }
+
+    @Test
+    public void addLogin() {
+    }
+
+    @Test
+    public void findAllLoginsStartingWith() {
+    }
+
+    @Test
+    public void findAllLogins() {
+    }
+}
